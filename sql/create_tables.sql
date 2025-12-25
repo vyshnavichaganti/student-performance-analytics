fa6a541b8feb0e1da942d1f5258f8d795ba48792
Vyshnavi <yourgmail@example.com>
2025-12-25 21:41:16 +0530

    Initial commit - Student Performance Analytics project

---
+++ sql/create_tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE students (
+    gender TEXT,
+    nationality TEXT,
+    place_of_birth TEXT,
+    raisedhands INT,
+    visited_resources INT,
+    announcements_view INT,
+    discussion INT,
+    student_absence_days TEXT,
+    class TEXT
+);
